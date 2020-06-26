98c1922a2585975bbafeafd338555c907bbcf763
Samuel Suh <samsuh@gmail.com>
2020-06-25 22:25:27 -0700

    added react-router-dom and set up BrowserRouter and routing rules in App.js, as well as creating empty components for routes

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventDetailsPage from "../../features/event/EventDetailsPage/EventDetailsPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailsPage from "../../features/user/UserDetailsPage/UserDetailsPage";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
 
 class App extends Component {
   render() {
@@ -9,7 +16,13 @@ class App extends Component {
       <>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/events" component={EventDashboard} />
+          <Route path="/events/:id" component={EventDetailsPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/profile/:id" component={UserDetailsPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventForm} />
         </Container>
       </>
     );
