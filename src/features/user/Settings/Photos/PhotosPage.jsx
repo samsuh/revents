3725e5ae543249f5c4cf774a32e1d6ef21ddd68c
Samuel Suh <samsuh@gmail.com>
2020-07-11 01:22:50 -0700

    add photo crop and upload to firestore via firebase

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import {
   Image,
   Segment,
@@ -9,15 +10,39 @@ import {
   Card,
 } from "semantic-ui-react";
 import DropzoneInput from "./DropzoneInput";
+import CropperInput from "./CropperInput";
+import { uploadProfileImage } from "../../userActions";
+import { toastr } from "react-redux-toastr";
 
-const PhotosPage = () => {
+const actions = {
+  uploadProfileImage,
+};
+
+const PhotosPage = ({ uploadProfileImage }) => {
   const [files, setFiles] = useState([]);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     return () => {
       files.forEach((file) => URL.revokeObjectURL(file.preview));
     };
   }, [files]);
+
+  const handleUploadImage = async () => {
+    try {
+      await uploadProfileImage(image, files[0].name);
+      handleCancelCrop();
+      toastr.success("Success", "Photo has been uploaded");
+    } catch (error) {
+      console.log(error);
+      toastr.error("Oops", "Something went wrong");
+    }
+  };
+
+  const handleCancelCrop = () => {
+    setFiles([]);
+    setImage(null);
+  };
 
   return (
     <Segment>
@@ -31,15 +56,37 @@ const PhotosPage = () => {
         <Grid.Column width={1} />
         <Grid.Column width={4}>
           <Header sub color="teal" content="Step 2 - Resize image" />
+          {files.length > 0 && (
+            <CropperInput setImage={setImage} imagePreview={files[0].preview} />
+          )}
         </Grid.Column>
         <Grid.Column width={1} />
         <Grid.Column width={4}>
           <Header sub color="teal" content="Step 3 - Preview & Upload" />
           {files.length > 0 && (
-            <Image
-              src={files[0].preview}
-              style={{ minHeight: "200px", minWidth: "200px" }}
-            />
+            <>
+              <div
+                className="img-preview"
+                style={{
+                  minHeight: "200px",
+                  minWidth: "200px",
+                  overflow: "hidden",
+                }}
+              />
+              <Button.Group>
+                <Button
+                  onClick={handleUploadImage}
+                  style={{ width: "100px" }}
+                  positive
+                  icon="check"
+                />
+                <Button
+                  onClick={handleCancelCrop}
+                  style={{ width: "100px" }}
+                  icon="close"
+                />
+              </Button.Group>
+            </>
           )}
         </Grid.Column>
       </Grid>
@@ -67,4 +114,4 @@ const PhotosPage = () => {
   );
 };
 
-export default PhotosPage;
+export default connect(null, actions)(PhotosPage);
--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -1,4 +1,9 @@
 import { toastr } from "react-redux-toastr";
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from "../async/asyncActions";
 
 export const updateProfile = (user) => async (
   dispatch,
@@ -14,5 +19,53 @@ export const updateProfile = (user) => async (
     toastr.success("Success", "Your profile has been updated");
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: fileName,
+  };
+  try {
+    dispatch(asyncActionStart());
+    //upload the file to firebase storage (from getFirebase instance)
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    //get url of image
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+    //get userdocument
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    //check if user has photo. if not, update profile in firestore
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL,
+      });
+      await user.updateProfile({
+        photoURL: downloadURL,
+      });
+    }
+    //add image to firestore
+    await firestore.add(
+      {
+        collection: "users",
+        doc: user.uid,
+        subcollections: [{ collection: "photos" }],
+      },
+      {
+        name: fileName,
+        url: downloadURL,
+      }
+    );
+    dispatch(asyncActionFinish);
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError);
   }
 };
